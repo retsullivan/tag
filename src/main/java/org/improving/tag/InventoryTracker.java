32722512205a3110a2e147cf2ef588c75a0d3b6a
Rachel Sullivan <Rachel.Sullivan@improving.com>
2019-09-24 08:06:33 -0500

    put my inventory tracker back in to the TAGwith Set Name

--- src/main/java/org/improving/tag/InventoryTracker.java
+++ src/main/java/org/improving/tag/InventoryTracker.java
@@ -1,4 +1,65 @@
-package org.improving.tag;
+/*package org.improving.tag;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InventoryTracker {
-}
+
+    // HashMap with arrayLists of items by ID.
+    public static HashMap<Integer, ArrayList<String>> items = new HashMap<Integer, ArrayList<String>>();
+    // Backpack with a slightly different structure.
+    public static HashMap<Integer, String> backpack = new HashMap<Integer, String>();
+
+    public static void main(String[] args) {
+
+        // Creating the arrayLists.
+        ArrayList<String> books = new ArrayList<String>();
+        ArrayList<String> weapons = new ArrayList<String>();
+        ArrayList<String> food = new ArrayList<String>();
+        ArrayList<String> medicine = new ArrayList<String>();
+        ArrayList<String> puppies = new ArrayList<String>();
+
+        // Adding content to them...
+        for (int i = 0; i < 10; i++) {
+            books.add("Books.toString());
+                    weapons.add("Weapons.toString());
+                            food.add("Food.toString());
+                                    medicine.add("Medicine.toString());
+                                            puppies.add("Puppies.toString());
+        }
+
+        // Putting the arrayLists to the HashMap of items by their IDs.
+        items.put(ID.books, books); items.put(ID.weapons, weapons);
+        items.put(ID.food, food); items.put(ID.medicine, medicine);
+        items.put(ID.puppies, puppies);
+
+        // Printing all the content in all arrayLists in all IDs.
+        for (ArrayList<String> e : items.values()) {
+            for (int i = 0; i < e.size(); i++) {
+                if (e.get(i)!=null) {
+                    System.out.println(e.get(i));
+                }
+            }
+        }
+
+        // Here you could add any item to the backpack.
+        backpack.put(ID.books, books.get(0));
+        backpack.put(ID.weapons, weapons.get(1));
+        backpack.put(ID.food, food.get(2));
+        backpack.put(ID.medicine, medicine.get(3));
+        backpack.put(ID.puppies, puppies.get(4));
+    }
+
+    public class ID {
+
+        //Defining constant-IDs.
+        public static final int books = 0;
+        public static final int weapons = 1;
+        public static final int food = 2;
+        public static final int medicine = 3;
+        public static final int puppies = 4;
+
+    }
+
+}*/
+
+
--- src/test/java/InventoryTrackerTest.java
+++ src/test/java/InventoryTrackerTest.java
@@ -1,2 +1,34 @@
+/*
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.improving.tag.commands.InventoryCommand;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class InventoryTrackerTest {
-}
+
+
+    public class InventoryCommandTests {
+
+        InventoryCommand target;
+        TestInputOutput io;
+
+        @BeforeEach
+        public void arrange(){
+            io = new TestInputOutput();
+            target = new InventoryCommand(io);
+        }
+
+        @Test
+        public void isValid_should_be_true_when_input_is_inventory(){
+            //Act
+            var result = target.isValid("inventory");
+            //Assert
+            assertTrue(result);
+
+
+
+
+        }
+*/
